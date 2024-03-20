375c7822bf8d07f9439ca47095495ecc47ec5c25
PPqtae <in010kim4121@outlook.kr>
2024-03-20 17:04:18 +0900

    a

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.io.FileOutputStream;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        /*(int number = scanner.nextInt();
+        System.out.print("number: " + number);*/
+
+       // String name = scanner.nextLine(); //nextLine은 공백까지도 받아준다 안적는다면 공백너머는 출력이 안됌
+       // System.out.print("name: " + name);
+
+       // int a = scanner.nextInt();
+       // int b = scanner.nextInt();
+
+       // if (a > b){
+        //    System.out.println("a가 더 큽니다");
+
+      //  for(int i = 0; i < 5; i++) {
+        //    System.out.println(i);\
+
+        boolean a = false;
+        while(a) {
+            System.out.println("1");
+        }
+
+
+    }
+}
